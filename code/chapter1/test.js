6a479187084ec1edf7e470722dcb572b478d5204
linbo <tjuser@tjuserdeMacBook-Pro.local>
2019-09-01 19:55:46 +0800

    add Image

--- code/chapter1/test.js
+++ code/chapter1/test.js
@@ -1,2 +1,29 @@
-const buf4 = Buffer.from([100000000, 2, 3]);
-console.log(buf4);
+const buf = Buffer.alloc(4);
+num = "0x11";
+buf.writeInt8(num, 0);
+console.log(buf); //<Buffer 11 00 00 00>
+buf.writeInt16BE(num, 0);
+console.log(buf); //<Buffer 00 11 00 00>
+buf.writeInt32BE(num, 0);
+console.log(buf); //<Buffer 00 00 00 11>
+
+buf.writeInt32LE(num, 0);
+console.log(buf); //<Buffer 11 00 00 00>
+
+// buf.writeUInt16BE(num, 0);
+// console.log(buf); //buf.fill();
+
+// buf.writeInt16LE(num, 0);
+// console.log(buf); //buf.fill();
+
+// buf.writeUInt16LE(num, 0);
+// console.log(buf); //buf.fill();
+
+// buf.writeUInt32BE(num, 0);
+// console.log(buf); //buf.fill();
+
+// buf.writeInt32LE(num, 0);
+// console.log(buf); //buf.fill();
+
+// buf.writeUInt32LE(num, 0);
+// console.log(buf); //buf.fill();
